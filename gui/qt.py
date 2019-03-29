eb08a7abd054531d385041ae5c0b3b6a48fb3738
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2019-03-28 22:34:52 -0400

    Update qt.py

--- gui/qt.py
+++ gui/qt.py
@@ -5,5 +5,11 @@ if API == 'pyqt5':
     from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 else:
     from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg, NavigationToolbar2QT
+    
+if hasattr(Qt, 'AA_EnableHighDpiScaling'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+ 
+if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
 __all__ = ['QtGui', 'QtCore', 'QtWidgets', 'Qt', 'FigureCanvasQTAgg', 'NavigationToolbar2QT']
